f39bb9ed24dda5f2e423d434b572306ee6bba6ee
Logan M Hood <hoodlm@protonmail.com>
2025-04-22 21:19:48 -0400

    Build/install scripts

---
+++ build.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+fail() {
+  msg=$1
+  echo "error: $msg" > /dev/stderr
+  exit 1
+}
+
+info() {
+  msg=$1
+  echo "info: $msg" > /dev/stderr
+}
+
+check_dependency() {
+  dep=$1
+  command -v "$dep" || fail "dependency $dep is not installed or not on PATH"
+}
+
+check_dependency "7z"
+
+BASEDIR=$(dirname $0)
+BUILDDIR="$BASEDIR/build"
+
+mkdir --parents --verbose "$BUILDDIR"
+
+FILES="
+    CITY.TXT
+    EVENTS.TXT
+    NOVA.scn
+    NOVA.txt
+    RULES.TXT
+    TERRAIN1.GIF
+    TERRAIN2.GIF
+    UNITS.GIF
+"
+for FILE in $FILES; do
+  install --preserve-timestamps --target-directory="$BUILDDIR" --verbose "$FILE"
+done
+
+7z a "EVN.7z" "$BUILDDIR/*"
---
+++ install.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eu -o pipefail
+
+CIV2_PATH="$HOME/.wine/drive_c/Program Files (x86)/MicroProse Software/Civilization II Multiplayer Gold Edition"
+
+fail() {
+  msg=$1
+  echo "error: $msg" > /dev/stderr
+  exit 1
+}
+
+info() {
+  msg=$1
+  echo "info: $msg" > /dev/stderr
+}
+
+check_dependency() {
+  dep=$1
+  command -v "$dep" || fail "dependency $dep is not installed or not on PATH"
+}
+
+check_dependency "7z"
+
+test -d "$CIV2_PATH" || fail "CIV2_PATH is not a valid directory: $CIV2_PATH"
+test -d "$CIV2_PATH/SCENARIO" || fail "Could not find SCENARIO subdirectory at $CIV2_PATH"
+
+OUTDIR="$CIV2_PATH/SCENARIO/EVN"
+mkdir --parents --verbose "$OUTDIR"
+
+7z e -o"$OUTDIR" "EVN.7z"
+
+set -x
+ls -l "$OUTDIR"
+set +x
+
+info "Successfully installed EVN at $OUTDIR"
